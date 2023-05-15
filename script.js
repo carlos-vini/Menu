0867baf490e45383c9e077b99de321194cff2faa
Carlos Vinícius <107522005+carlos-vini@users.noreply.github.com>
2023-05-15 12:43:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,90 +2,159 @@ const menu = [
     {
         id: 1,
         title: 'Buttermilk Pancakes',
-        category: 'Breakfast', 
+        category: 'Breakfast',
         price: 15.99,
-        image: "./images/item-1.jpeg",
-        description: "I'm baby woke mlkshk wolf bitters levi edge blue bottle, hammock freegan copper whatever cold-pressed",
+        image: './images/item-1.jpeg',
+        description: "I'm baby woke milshake wolf bitters live-edge blue bottle, hamrock freegan copper whatever cold-pressed.",
     },
 
     {
         id: 2,
         title: 'Diner Double',
-        category: 'Lunch', 
-        price: 13.99,
-        image: "./images/item-2.jpeg",
+        category: 'Lunch',
+        price: 12.99,
+        image: './images/item-2.jpeg',
         description: "Vaporware iphone mumnlecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing.",
     },
 
     {
         id: 3,
         title: 'Godzilla Milkshake',
-        category: 'Shakes', 
+        category: 'Shakes',
         price: 6.99,
-        image: "./images/item-3.jpeg",
-        description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral",
+        image: './images/item-3.jpeg',
+        description: "Ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
     },
 
     {
         id: 4,
         title: 'Country Delight',
-        category: 'Breakfast', 
+        category: 'Breakfast',
         price: 20.99,
-        image: "./images/item-4.jpeg",
-        description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut",
+        image: './images/item-4.jpeg',
+        description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch.Prism austin mlkshk truffaut.",
     },
 
     {
         id: 5,
         title: 'Egg Attack',
-        category: 'Lunch', 
+        category: 'Lunch',
         price: 22.99,
-        image: "./images/item-5.jpeg",
-        description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to- table 90's pop-up",
+        image: './images/item-5.jpeg',
+        description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up.",
     },
 
     {
         id: 6,
         title: 'Oreo Dream',
-        category: 'Shakes', 
+        category: 'Shakes',
         price: 18.99,
-        image: "./images/item-6.jpeg",
-        description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral",
+        image: './images/item-6.jpeg',
+        description: "Portland chicharrones ethical edison, palo santo craft beer chia heirloom iphone everyday.",
     },
 
     {
         id: 7,
         title: 'Bacon Overflow',
-        category: 'Breakfast', 
+        category: 'Breakfast',
         price: 8.99,
-        image: "./images/item-7.jpeg",
-        description: "Portland chicharrones ethical edison, palo santo craft beer chia heirloom iphone everyday",
+        image: './images/item-7.jpeg',
+        description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird.",
     },
 
     {
         id: 8,
         title: 'American Classic',
-        category: 'Lunch', 
+        category: 'Lunch',
         price: 12.99,
-        image: "./images/item-8.jpeg",
-        description: "On it tumblr kickstarter thundercast migas everyday carry squid palo santo leggings. Food truck truffaut",
+        image: './images/item-8.jpeg',
+        description: "On it tumblr kickstarter thundercast migas everyday carry squid palo santo leggings. Food truck truffaut.",
     },
 
     {
         id: 9,
         title: 'Quarantine Buddy',
-        category: 'Shakes', 
+        category: 'Shakes',
         price: 16.99,
-        image: "./images/item-9.jpeg",
-        description: "Skateboard fam snth authentic semiotics. Live-edge lyft af, edison buld",
+        image: './images/item-9.jpeg',
+        description: "Skateboard fam synth authentic semiotics. Live-edge lyft af, edison buld yuccie crucifix microdosing.",
     },
 
     {
         id: 10,
         title: 'Stack Dinner',
-        category: 'Dinner', 
+        category: 'Dinner',
         price: 39.99,
-        image: "./images/item-10.jpeg",
-        description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral",
+        image: './images/item-10.jpeg',
+        description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up.",
     },
-]
+];
+let menuItems = document.querySelector('.menu-itens')
+let buttonContainer = document.querySelector('.button-container')
+
+window.addEventListener("DOMContentLoaded", () => {
+    displayMenuItems(menu);
+    displayMenuButtons();
+})
+ function displayMenuItems(menuItem) {
+    let showMenu = menuItem.map((menu) => {
+        return `
+        <div>
+            <div class="item-image">
+                            <img src="${menu.image}" alt="${menu.title}" srcset="">
+                        </div>
+                        <div class="item-info">
+                            <div class="item-title">
+                                <p>${menu.title}</p>
+                                <p class="price">R$${menu.price}</p>
+                            </div>
+                            <div class="item-description">
+                                <p>${menu.description}</p>
+                            </div>
+                        </div>
+            </div>
+    
+            `
+    });
+    showMenu = showMenu.join("");
+    menuItems.innerHTML = showMenu;
+ }
+
+function displayMenuButtons() {
+    const categories = menu.reduce((value, item) => {
+        if(!value.includes(item.category)){
+            value.push(item.category)
+        }
+
+        return value;
+    }, ["all"]);
+
+    const categoryButtons = categories.map((category) => {
+        return `
+        <button class="filter-button" data-id="${category}">${category}</button>
+        `
+    }).join("");
+
+    buttonContainer.innerHTML += categoryButtons;
+
+const filterButton = document.querySelectorAll(".filter-button");
+
+filterButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const category = event.currentTarget.dataset.id;
+        const menuCategory = menu.filter((menuItem) => {
+            if(menuItem.category === category) {
+                return menuItem;
+            }
+        });
+
+        if(category === "all"){
+            displayMenuItems(menu);
+        } else {
+            displayMenuItems(menuCategory);
+        };
+    });
+});
+
+}
+''
